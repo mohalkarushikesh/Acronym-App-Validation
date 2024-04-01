36ac7f69002e7fbf80c05313cbb3a06f61b1b343
Mohalkar <2327238@cognizant.com>
2024-04-01 19:55:26 +0530

    fifth commit

--- src/test/java/userDefinedLibraries/ScreenShot.java
+++ src/test/java/userDefinedLibraries/ScreenShot.java
@@ -1,5 +1,45 @@
 package userDefinedLibraries;
 
-public class ScreenShot {
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class ScreenShot {
+	public static String filepath = ".\\Screenshots\\";
+
+	public static String screenShotTC(WebDriver scdriver, String screenShotName) {
+		TakesScreenshot ts = (TakesScreenshot) scdriver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh-mm-ss");
+		Date date = new Date();
+		String dateValue = dateFormat.format(date);
+		String strdestination = System.getProperty("user.dir") + "/Screenshots/" + screenShotName + "_" + dateValue
+				+ ".png";
+		File destination = new File(strdestination);
+		try { // now copy the screenshot to desired location using copyFile method
+			FileUtils.copyFile(source, destination);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return strdestination;
+	}
 }
+
+
+/*
+to take screenshot
+
+try {
+		// Take screen shot
+		ScreenShot.screenShotTC(driver, "Ads and notifications");
+	} catch (Exception e) {
+		e.printStackTrace();
+}
+
+*/
